d29822609d0e8bd0af70fca8cbc6da1c556e9765
SoClear <36636341@qq.com>
2020-04-04 18:04:58 +0800

    添加了一些总结

---
+++ src/main/kotlin/p2_intermediate/p1_mappingType/p6_总结.kt
@@ -0,0 +1,9 @@
+package p2_intermediate.p1_mappingType
+
+/*
+本章有以下重点:
+●映射是由键值对组成的数据结构。
+●映射分为可变映射和不可变映射。
+●不可变映射底层实现为Java的LinkedHashMap类型。
+●可变映射底层实现有Java的HashMap类型、LinkedHashMap类型和TreeMap类型。
+ */
